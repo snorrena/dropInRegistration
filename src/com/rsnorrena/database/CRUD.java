9dc955343897f6033fa78a3517e47c82a96b0c75
Scott Norrena <snorrena@gmail.com>
2022-01-05 15:16:18 -0800

    update the db connection string

--- src/com/rsnorrena/database/CRUD.java
+++ src/com/rsnorrena/database/CRUD.java
@@ -13,66 +13,58 @@ import org.apache.log4j.Logger;
 import com.rsnorrena.data.Player;
 
 public class CRUD {
-	
+
 	static Logger log = Logger.getLogger(CRUD.class);
-	
-	//Declare the sqlite connect to be static to insure that there is only one connection
+
+	// Declare the sqlite connect to be static to insure that there is only one
+	// connection
 	private static Connection conn;
 
 	public static Connection dbConnection() {
 		try {
 			if (conn == null || conn.isClosed()) {
-				
-//				File f = new File("myFile.txt");
-//				System.out.println(f.getAbsolutePath());
-//				
+
 				Class.forName("org.sqlite.JDBC");
-				
+
 				String jdbc = "jdbc:sqlite:";
 
-				//path for database file on windows
-				String path = "C:\\Github\\Java\\Eclipse\\2020-12\\program_archive\\dropInRegistration\\";
-				
-				//path for database file on windows
-//				String path = "/home/snorrena/Desktop/Github/Java/Eclipse/2021-09/program_archive/dropInRegistration/";
+				// path for database file on windows
+				String path = "C:/Github/Java/Eclipse/2021-09/program_archive/dropInRegistration/";
 
-				//path for docker container
-				//String path = "/home/dropInRegistration/";
-				
+				// path for docker container
+				// String path = "/home/dropInRegistration/";
+
 				String fileName = "PlayerInfo.db";
 
 				String url = jdbc + path + fileName;
-				
+
 				conn = DriverManager.getConnection(url);
-				
+
 			}
 
 			return conn;
 
 		} catch (Exception e) {
-			
+
 			return null;
 		}
 
 	}
-	
-	public static boolean updateRecord(String firstName, String lastName, String password, boolean isRegistered, String email,
-			String playerPosition, String playerStatus, int index){
-		
+
+	public static boolean updateRecord(String firstName, String lastName, String password, boolean isRegistered,
+			String email, String playerPosition, String playerStatus, int index) {
+
 		log.info("updateRecord called");
-		
+
 		int reg = isRegistered ? 1 : 0;
 
 		conn = null;
-		
 
 		PreparedStatement stm = null;
-		
 
-		String msql = "UPDATE playerlist SET firstname = ?, "
-				+ "lastname = ?, password = ?, isregistered = ?, "
+		String msql = "UPDATE playerlist SET firstname = ?, " + "lastname = ?, password = ?, isregistered = ?, "
 				+ "position = ?, status = ?, index_no = ? WHERE email = ?";
-		
+
 		conn = dbConnection();
 
 		try {
@@ -92,34 +84,36 @@ public class CRUD {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
-				stm.close();
-				conn.close();
+				if (conn != null) {
+					stm.close();
+					conn.close();
+				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return true;
 	}
 
-	public static void insertRecord(String firstName, String lastName, String password,
-			boolean isRegistered, String email, String position, String status, int index) {
-		
+	public static void insertRecord(String firstName, String lastName, String password, boolean isRegistered,
+			String email, String position, String status, int index) {
+
 		log.info("insertRecord called");
-		
+
 		int reg = isRegistered ? 1 : 0;
-		
+
 		conn = null;
 		PreparedStatement stm = null;
-		
+
 //		String sql = "INSERT INTO Employees VALUES (" + employeeId + ", '" + name + "', '" + surname + "', '" + userName + "','" + password + "', " + age + ")";
 
-		String sql ="INSERT INTO playerlist "
-				+ "(firstname, lastname, password, isregistered, email, position, status, index_no) " 
+		String sql = "INSERT INTO playerlist "
+				+ "(firstname, lastname, password, isregistered, email, position, status, index_no) "
 				+ "VALUES (?,?,?,?,?,?,?,?)";
-		
+
 		conn = dbConnection();
 
 		try {
@@ -135,10 +129,10 @@ public class CRUD {
 			stm.setInt(8, index);
 			stm.executeUpdate();
 			conn.commit();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				stm.close();
 				conn.close();
@@ -147,65 +141,63 @@ public class CRUD {
 			}
 		}
 	}
-	
-	public static Player getRecord(String email){
-		
+
+	public static Player getRecord(String email) {
+
 		log.info("getRecord called");
-		
+
 		conn = null;
-		
+
 		Statement stm = null;
 		ResultSet rs = null;
-		
-		Player currentPlayer = new Player();
-		
-		//Here we check the value if the player email exists in the database. 
-//		if (!playerCheck(email)){
-			
-			
-			String query = "SELECT * FROM playerlist WHERE email = '" + email + "'";
-			
-			conn = dbConnection();
 
-				try {
-					stm = conn.createStatement();
-					rs = stm.executeQuery(query);
-								
-					currentPlayer.setFirstName(rs.getString("firstname"));
-					currentPlayer.setLastName(rs.getString("lastname"));
-					currentPlayer.setPassword(rs.getString("password"));
-					currentPlayer.setEmailAddress(rs.getString("email"));
-					currentPlayer.setRegistered((rs.getInt("isregistered"))== 1 ? true : false);
-					currentPlayer.setPosition(rs.getString("position"));
-					currentPlayer.setStatus(rs.getString("status"));
-					currentPlayer.setIndex(rs.getInt("index_no"));
-					
-								
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}finally{
-					try {
-						stm.close();
-						rs.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-		
+		Player currentPlayer = new Player();
+
+		// Here we check the value if the player email exists in the database.
+//		if (!playerCheck(email)){
+
+		String query = "SELECT * FROM playerlist WHERE email = '" + email + "'";
+
+		conn = dbConnection();
+
+		try {
+			stm = conn.createStatement();
+			rs = stm.executeQuery(query);
+
+			currentPlayer.setFirstName(rs.getString("firstname"));
+			currentPlayer.setLastName(rs.getString("lastname"));
+			currentPlayer.setPassword(rs.getString("password"));
+			currentPlayer.setEmailAddress(rs.getString("email"));
+			currentPlayer.setRegistered((rs.getInt("isregistered")) == 1 ? true : false);
+			currentPlayer.setPosition(rs.getString("position"));
+			currentPlayer.setStatus(rs.getString("status"));
+			currentPlayer.setIndex(rs.getInt("index_no"));
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stm.close();
+				rs.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return currentPlayer;
 	}
 
 	public static int getRegisteredCount() {
-		
+
 		log.info("getRegisteredCount called");
-		
+
 		conn = null;
 		int registeredCount = 0;
-		
+
 		Statement stm = null;
 		ResultSet rs = null;
-		
+
 		try {
 			/**
 			 * Get a connection to the sqlite database and create a statement
@@ -219,12 +211,12 @@ public class CRUD {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			try{
+		} finally {
+			try {
 				stm.close();
 				rs.close();
 				conn.close();
-			}catch (Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
@@ -232,9 +224,9 @@ public class CRUD {
 	}
 
 	public static int login(String userName, String passWord) {
-		
+
 		log.info("login called");
-		
+
 		conn = null;
 		String query = "select * from playerlist where email = ? and password = ?";
 		int count = 0;
@@ -253,11 +245,13 @@ public class CRUD {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
-				rs.close();
-				pst.close();
-				conn.close();
+				if (conn != null) {
+					rs.close();
+					pst.close();
+					conn.close();
+				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
@@ -266,32 +260,31 @@ public class CRUD {
 	}
 
 	public static boolean playerCheck(String email) {
-		
+
 		log.info("playerCheck called");
 
 		conn = null;
-		
+
 		Statement stm = null;
 		ResultSet rs = null;
-		
+
 		boolean emailExistsInDB = true;
-		
+
 		String sql = "SELECT * FROM playerlist WHERE Email = " + "'" + email + "'";
-		
+
 		conn = dbConnection();
-		
+
 		try {
 			stm = conn.createStatement();
 			rs = stm.executeQuery(sql);
 
-					if (!rs.next() ) {    
-					    emailExistsInDB = false;
-					} 
-			
-			
+			if (!rs.next()) {
+				emailExistsInDB = false;
+			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				rs.close();
 				stm.close();
@@ -299,23 +292,21 @@ public class CRUD {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
-		
-		
+
 		return emailExistsInDB;
 	}
 
 	public static boolean deleteRecord(String email) {
-		
+
 		log.info("deleteRecord called");
-		
+
 		boolean recordDeleted = false;
 		conn = null;
 		Statement stm = null;
 		String sql = "DELETE FROM playerlist WHERE email = " + "'" + email + "'";
-		
+
 		conn = dbConnection();
 		try {
 			conn.setAutoCommit(false);
@@ -325,7 +316,7 @@ public class CRUD {
 			recordDeleted = true;
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				stm.close();
 				conn.close();
@@ -335,99 +326,95 @@ public class CRUD {
 		}
 		return recordDeleted;
 	}
-	
-public static ArrayList<Player> getAllPlayers(){
-	
-	log.info("getAllPlayers called");
-		
+
+	public static ArrayList<Player> getAllPlayers() {
+
+		log.info("getAllPlayers called");
+
 		conn = null;
-		
+
 		Statement stm = null;
 		ResultSet rs = null;
-		
+
 		ArrayList<Player> allPlayers = new ArrayList<Player>();
-		
-			String query = "SELECT * FROM playerlist";
-			
-			conn = dbConnection();
-			
-				try {
-					stm = conn.createStatement();
-					rs = stm.executeQuery(query);
-					
-					while (rs.next()){
-						
-							
-							Player currentPlayer = new Player();
-							
-							currentPlayer.setFirstName(rs.getString("firstname"));
-							currentPlayer.setLastName(rs.getString("lastname"));
-							currentPlayer.setPassword(rs.getString("password"));
-							currentPlayer.setRegistered((rs.getInt("isregistered"))== 1 ? true : false);
-							currentPlayer.setEmailAddress(rs.getString("email"));
-							currentPlayer.setPosition(rs.getString("position"));
-							currentPlayer.setStatus(rs.getString("status"));
-							currentPlayer.setIndex(rs.getInt("index_no"));
-							
-							allPlayers.add(currentPlayer);
-							
-						}
-						
-								
-								
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}finally{
-					try {
-						stm.close();
-						rs.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-		
+
+		String query = "SELECT * FROM playerlist";
+
+		conn = dbConnection();
+
+		try {
+			stm = conn.createStatement();
+			rs = stm.executeQuery(query);
+
+			while (rs.next()) {
+
+				Player currentPlayer = new Player();
+
+				currentPlayer.setFirstName(rs.getString("firstname"));
+				currentPlayer.setLastName(rs.getString("lastname"));
+				currentPlayer.setPassword(rs.getString("password"));
+				currentPlayer.setRegistered((rs.getInt("isregistered")) == 1 ? true : false);
+				currentPlayer.setEmailAddress(rs.getString("email"));
+				currentPlayer.setPosition(rs.getString("position"));
+				currentPlayer.setStatus(rs.getString("status"));
+				currentPlayer.setIndex(rs.getInt("index_no"));
+
+				allPlayers.add(currentPlayer);
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stm.close();
+				rs.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return allPlayers;
 	}
 
-public static boolean updateRegistration(String email, boolean isRegistered, String status, int index){
-	
-	log.info("updatRegistration called");
-	
-	int reg = isRegistered ? 1 : 0;
+	public static boolean updateRegistration(String email, boolean isRegistered, String status, int index) {
 
-	conn = null;
-	
-	PreparedStatement stm = null;
-	
-	String msql = "UPDATE playerlist SET isregistered = ?, status = ?, index_no = ? WHERE email = ?";
-	
-	conn = dbConnection();
+		log.info("updatRegistration called");
 
-	try {
+		int reg = isRegistered ? 1 : 0;
 
-		conn.setAutoCommit(false);
-		stm = conn.prepareStatement(msql);
-		stm.setInt(1, reg);
-		stm.setString(2, status);
-		stm.setInt(3, index);
-		stm.setString(4, email);
-		stm.executeUpdate();
-		conn.commit();
+		conn = null;
 
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}finally{
+		PreparedStatement stm = null;
+
+		String msql = "UPDATE playerlist SET isregistered = ?, status = ?, index_no = ? WHERE email = ?";
+
+		conn = dbConnection();
+
 		try {
-			stm.close();
-			conn.close();
+
+			conn.setAutoCommit(false);
+			stm = conn.prepareStatement(msql);
+			stm.setInt(1, reg);
+			stm.setString(2, status);
+			stm.setInt(3, index);
+			stm.setString(4, email);
+			stm.executeUpdate();
+			conn.commit();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				stm.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-	}
-	
-	return true;
-}
 
+		return true;
+	}
 
 }
